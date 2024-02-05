5a6aba2da429fed7272758e7d13f5a397bac8b7a
Pablo Rivero <pablo.rivero1@usp.ceu.es>
2024-02-05 16:26:54 +0100

    added list component

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,13 +1,58 @@
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>My Hacker Stories</h1>
 
+      <Search />
+      <List />
+    </div>
+  );
+} 
 
+function Search(){
+  return(
+    <div>
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' />
     </div>
+    
   );
 }
 
+function List(){
+  return(
+    <ul>
+        {list.map(function (item) {
+          return <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span><br />
+          <span>Author: {item.author}</span><br />
+          <span>Number of comments: {item.num_comments}</span><br />
+          <span>Points: {item.points}</span><br />
+          </li> ;
+        })}
+      </ul>
+  )
+}
 export default App
