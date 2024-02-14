a31eabefbf9399cb3448fd0a51af4eb3710f0388
Pablo Rivero <pablo.rivero1@usp.ceu.es>
2024-02-14 23:05:53 +0100

    added props to pass down information

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,58 +1,89 @@
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+import PropTypes from 'prop-types';
 
-
-function App() {
+const App = () =>  {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
       <Search />
-      <List />
+      <List list={stories}/>
     </div>
-  );
-} 
+  )};
 
-function Search(){
+const Search = () => {
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+  }
   return(
     <div>
       <label htmlFor='search'>Search: </label>
-      <input id='search' type='text' />
+      <input id='search' type='text' onChange={handleChange}/>
     </div>
-    
-  );
-}
 
-function List(){
-  return(
+  )};
+
+const List = (props) => (
     <ul>
-        {list.map(function (item) {
-          return <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span><br />
-          <span>Author: {item.author}</span><br />
-          <span>Number of comments: {item.num_comments}</span><br />
-          <span>Points: {item.points}</span><br />
-          </li> ;
-        })}
-      </ul>
-  )
-}
+      {props.list.map((item) => (
+        <Item key={item.objectID} item={item}/>)
+      )}
+    </ul>
+);
+
+const Item = (props) => (
+  <li>
+    <span>
+    <a href={props.item.url}>{props.item.title}</a>
+    </span><br />
+    <span>Author: {props.item.author}</span><br />
+    <span>Number of comments: {props.item.num_comments}</span><br />
+    <span>Points: {props.item.points}</span><br />
+  </li>
+)
+
+
+List.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+      num_comments: PropTypes.number.isRequired,
+      points: PropTypes.number.isRequired,
+      objectID: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+Item.propTypes = {
+  item: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    num_comments: PropTypes.number.isRequired,
+    points: PropTypes.number.isRequired,
+    objectID: PropTypes.number.isRequired,
+  }).isRequired,
+};
 export default App
