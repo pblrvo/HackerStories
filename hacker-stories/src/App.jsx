171ef9100c44bd880ea3d6d6926034e8377b0dfb
pblrvo <pblrvo@gmail.com>
2024-03-20 23:20:40 +0100

    Added real data fecthing from hackernews API, also merged isLoading, isError into a single useReducer

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -2,46 +2,42 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const App = () =>  {
-  const initialStories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
 
-  const getAsyncStories = () => 
-     new Promise((resolve) =>
-      setTimeout(
-        () => resolve({data: {stories: initialStories}}),
-        2000)
-     );
+  const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
   const storiesReducer = (state, action) => {
     switch (action.type){
-      case 'SET_STORIES':
-        return action.payload
+      case 'STORIES_FETCH_INIT':
+        return{
+          ...state,
+          isLoading: true,
+          isError: false,
+        }
+
+      case 'STORIES_FETCH_SUCCESS':
+      return{
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      }
+      case 'STORIES_FETCH_FAILURE':
+      return{
+        ...state,
+        isLoading: false,
+        isError: true,
+      }
 
       case 'REMOVE_STORY':
-        return state.filter(
-          (story) => action.payload !== story.objectID
-        );
+        return {
+          ...state,
+          data: state.data.filter(story => action.payload !== story.objectID),
+        };
 
       default:
         throw new Error();
+      }
     }
-  }
 
   const useStorageState = (key, initialState) =>{
     const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
@@ -53,24 +49,43 @@ const App = () =>  {
     return [value, setValue];
   };
 
-  const [searchTerm, setSearchTerm] = useStorageState('search', localStorage.search || '');
+  const [searchTerm, setSearchTerm] = useStorageState('search', localStorage.getItem('search') || '');
 
-  const [stories, dispatchStories] = React.useReducer(storiesReducer, []);
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, {
+    data: [], isLoading: false, isError: false
+  });
 
-  const [isLoading, setIsLoading] = React.useState(false);
-
-  const {isError, setError} = React.useState(false);
 
   React.useEffect(() => {
-    setIsLoading(true);
-    getAsyncStories().then(result => {
+    console.log('useEffect called with searchTerm:', searchTerm);
+    if (!searchTerm) return;
+
+    dispatchStories({type: 'STORIES_FETCH_INIT'});
+
+    const url = `${API_ENDPOINT}${searchTerm}`;
+    console.log('Fetching data from URL:', url);
+
+    fetch(url).then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json()
+    }).then(result => {
+      console.log(result)
+      if (!result || !result.hits || !Array.isArray(result.hits)) {
+        throw new Error('The data returned from the API is not in the expected format');
+      }
+
       dispatchStories({
-        type: 'SET_STORIES',
-        payload: result.data.stories,
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.hits,
       });
-      setIsLoading(false);
-    }).catch(() => setError(true));
-  }, []);
+    }).catch(error =>{
+      console.error('There was a problem with the fetch operation:', error);
+      dispatchStories({type: 'STORIES_FETCH_FAILURE'})
+    }
+    );
+  }, [searchTerm]);
 
   const handleRemoveStory = (objectID) => {
     dispatchStories({
@@ -82,22 +97,23 @@ const App = () =>  {
     setSearchTerm(event.target.value)
   }  
 
-  const searchedStories = stories.filter(function (story){
+  /*const searchedStories = stories.data.filter((story) => {
     return story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  })
+  })*/
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
       <InputWithLabel id="search" type="text" value={searchTerm} isFocused onInputChange={handleSearch}>Search: </InputWithLabel>
 
-      {isError && <p>Something went wrong...</p>}
+      {stories.isError && <p>Something went wrong...</p>}
 
-      {isLoading ? (
+      {stories.isLoading ? (
         <p>Loading...</p>
       ) : (
       <List 
-        list={searchedStories} 
+        list={stories.data} 
         onRemoveItem={handleRemoveStory}
       />
       )}
@@ -140,15 +156,16 @@ const Item = ({title, url, author, num_comments, points, objectID, onRemoveItem}
   return (
   <li>
     <span>
-    <a href={url}>{title}</a>
-    </span><br />
+    <a href={url}>{title}</a> 
+    </span> <span>
+      <button type='button' onClick={handleRemoveItem}>Remove {title}</button>
+    </span>
+    <br />
     <span>Author: {author}</span><br />
     <span>Number of comments: {num_comments}</span><br />
     <span>Points: {points}</span><br />
-    <span>
-      <button type='button' onClick={handleRemoveItem}>Remove {title}</button>
-    </span>
   </li>
+  
 )};
 
 InputWithLabel.propTypes = {
